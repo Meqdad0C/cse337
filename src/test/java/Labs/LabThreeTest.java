67ce1e9d3fd2c86848a22d72333233c99bc8444d
Abdelmagid Mohamed <abdelmagidmohamed89@yahoo.com>
2024-03-22 17:24:10 +0200

    tested max,min average

--- src/test/java/Labs/LabThreeTest.java
+++ src/test/java/Labs/LabThreeTest.java
@@ -68,4 +68,264 @@ public class LabThreeTest {
         Assertions.assertEquals(-9, minValue);
 
     }
+	
+	
+    @Test
+    public void DataAnalyzerTest_GetMin3_negatives() throws IOException {
+        FileHandler fileHandler = mock(FileHandler.class);
+
+        Vector<Integer> numbers = new Vector<>();
+        numbers.add(-2);
+        numbers.add(-9);
+        numbers.add(-10);
+
+        when(fileHandler.GetData()).thenReturn(numbers);
+
+        DataAnalyzer da = new DataAnalyzer(fileHandler);
+        int minValue =  da.GetMin();
+        Assertions.assertEquals(-10, minValue);
+
+    }
+
+    @Test
+    public void DataAnalyzerTest_GetMin4_equal() throws IOException {
+        FileHandler fileHandler = mock(FileHandler.class);
+
+        Vector<Integer> numbers = new Vector<>();
+        numbers.add(5);
+        numbers.add(5);
+        numbers.add(5);
+
+        when(fileHandler.GetData()).thenReturn(numbers);
+
+        DataAnalyzer da = new DataAnalyzer(fileHandler);
+        int minValue =  da.GetMin();
+        Assertions.assertEquals(5, minValue);
+
+    }
+
+    @Test
+    public void DataAnalyzerTest_GetMin5_zero() throws IOException {
+        FileHandler fileHandler = mock(FileHandler.class);
+
+        Vector<Integer> numbers = new Vector<>();
+        numbers.add(0);
+        numbers.add(0);
+        numbers.add(0);
+
+        when(fileHandler.GetData()).thenReturn(numbers);
+
+        DataAnalyzer da = new DataAnalyzer(fileHandler);
+        int minValue =  da.GetMin();
+        Assertions.assertEquals(0, minValue);
+    }
+    @Test
+    public void DataAnalyzerTest_GetMin6_oneElement() throws IOException {
+        FileHandler fileHandler = mock(FileHandler.class);
+
+        Vector<Integer> numbers = new Vector<>();
+        numbers.add(20);
+
+
+        when(fileHandler.GetData()).thenReturn(numbers);
+
+        DataAnalyzer da = new DataAnalyzer(fileHandler);
+        int maxValue =  da.GetMax();
+        Assertions.assertEquals(20, maxValue);
+    }
+    @Test
+    public void DataAnalyzerTest_GetMax1_oneElement() throws IOException {
+        FileHandler fileHandler = mock(FileHandler.class);
+
+        Vector<Integer> numbers = new Vector<>();
+        numbers.add(20);
+
+
+        when(fileHandler.GetData()).thenReturn(numbers);
+
+        DataAnalyzer da = new DataAnalyzer(fileHandler);
+        int minValue =  da.GetMin();
+        Assertions.assertEquals(20, minValue);
+    }
+    @Test
+    public void DataAnalyzerTest_GetMax2() throws IOException {
+        FileHandler fileHandler = mock(FileHandler.class);
+
+        Vector<Integer> numbers = new Vector<>();
+        numbers.add(1);
+        numbers.add(6);
+        numbers.add(0);
+
+        when(fileHandler.GetData()).thenReturn(numbers);
+
+        DataAnalyzer da = new DataAnalyzer(fileHandler);
+        int maxValue =  da.GetMax();
+        Assertions.assertEquals(6, maxValue);
+    }
+
+    @Test
+    public void DataAnalyzerTest_GetMax3() throws IOException {
+        FileHandler fileHandler = mock(FileHandler.class);
+
+        Vector<Integer> numbers = new Vector<>();
+        numbers.add(1);
+        numbers.add(-9);
+        numbers.add(1);
+
+        when(fileHandler.GetData()).thenReturn(numbers);
+
+        DataAnalyzer da = new DataAnalyzer(fileHandler);
+        int maxValue =  da.GetMax();
+        Assertions.assertEquals(1, maxValue);
+
+    }
+
+    @Test
+    public void DataAnalyzerTest_GetMax4_negatives() throws IOException {
+        FileHandler fileHandler = mock(FileHandler.class);
+
+        Vector<Integer> numbers = new Vector<>();
+        numbers.add(-2);
+        numbers.add(-9);
+        numbers.add(-10);
+
+        when(fileHandler.GetData()).thenReturn(numbers);
+
+        DataAnalyzer da = new DataAnalyzer(fileHandler);
+        int maxValue =  da.GetMax();
+        Assertions.assertEquals(-2, maxValue);
+
+    }
+
+    @Test
+    public void DataAnalyzerTest_GetMax5_equal() throws IOException {
+        FileHandler fileHandler = mock(FileHandler.class);
+
+        Vector<Integer> numbers = new Vector<>();
+        numbers.add(5);
+        numbers.add(5);
+        numbers.add(5);
+
+        when(fileHandler.GetData()).thenReturn(numbers);
+
+        DataAnalyzer da = new DataAnalyzer(fileHandler);
+        int maxValue =  da.GetMax();
+        Assertions.assertEquals(5, maxValue);
+
+    }
+
+    @Test
+    public void DataAnalyzerTest_GetMax6_zero() throws IOException {
+        FileHandler fileHandler = mock(FileHandler.class);
+
+        Vector<Integer> numbers = new Vector<>();
+        numbers.add(0);
+        numbers.add(0);
+        numbers.add(0);
+
+        when(fileHandler.GetData()).thenReturn(numbers);
+
+        DataAnalyzer da = new DataAnalyzer(fileHandler);
+        int maxValue =  da.GetMax();
+        Assertions.assertEquals(0, maxValue);
+    }
+
+    @Test
+    public void DataAnalyzertest_GetAverage1_zero() throws IOException{
+        FileHandler fileHandler = mock(FileHandler.class);
+
+        Vector<Integer> numbers = new Vector<>();
+        numbers.add(0);
+        numbers.add(0);
+        numbers.add(0);
+
+        when(fileHandler.GetData()).thenReturn(numbers);
+
+        DataAnalyzer da = new DataAnalyzer(fileHandler);
+        int avgValue =  da.GetAverage();
+        Assertions.assertEquals(0, avgValue);
+
+    }
+    @Test
+    public void DataAnalyzertest_GetAverage2() throws IOException{
+        FileHandler fileHandler = mock(FileHandler.class);
+
+        Vector<Integer> numbers = new Vector<>();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+
+        when(fileHandler.GetData()).thenReturn(numbers);
+
+        DataAnalyzer da = new DataAnalyzer(fileHandler);
+        int avgValue =  da.GetAverage();
+        Assertions.assertEquals(2, avgValue);
+
+    }
+    @Test
+    public void DataAnalyzertest_GetAverage3() throws IOException{
+        FileHandler fileHandler = mock(FileHandler.class);
+
+        Vector<Integer> numbers = new Vector<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+
+        when(fileHandler.GetData()).thenReturn(numbers);
+
+        DataAnalyzer da = new DataAnalyzer(fileHandler);
+        int avgValue =  da.GetAverage();
+        Assertions.assertEquals(1, avgValue);
+
+    }
+
+    @Test
+    public void DataAnalyzertest_GetAverage4_negatives() throws IOException{
+        FileHandler fileHandler = mock(FileHandler.class);
+
+        Vector<Integer> numbers = new Vector<>();
+        numbers.add(-2);
+        numbers.add(-3);
+        numbers.add(-1);
+
+        when(fileHandler.GetData()).thenReturn(numbers);
+
+        DataAnalyzer da = new DataAnalyzer(fileHandler);
+        int avgValue =  da.GetAverage();
+        Assertions.assertEquals(-2, avgValue);
+
+    }
+    @Test
+    public void DataAnalyzertest_GetAverage5() throws IOException{
+        FileHandler fileHandler = mock(FileHandler.class);
+
+        Vector<Integer> numbers = new Vector<>();
+        numbers.add(5);
+        numbers.add(0);
+        numbers.add(-5);
+
+        when(fileHandler.GetData()).thenReturn(numbers);
+
+        DataAnalyzer da = new DataAnalyzer(fileHandler);
+        int avgValue =  da.GetAverage();
+        Assertions.assertEquals(0, avgValue);
+
+    }
+
+    @Test
+    public void DataAnalyzertest_GetAverage6() throws IOException{
+        FileHandler fileHandler = mock(FileHandler.class);
+
+        Vector<Integer> numbers = new Vector<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(4);
+
+        when(fileHandler.GetData()).thenReturn(numbers);
+
+        DataAnalyzer da = new DataAnalyzer(fileHandler);
+        int avgValue =  da.GetAverage();
+        Assertions.assertEquals(2, avgValue);
+
+    }
 }
