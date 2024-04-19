cd0fceb6c8dad1fbf308ffa35b8c04e813bf714f
MostafaMonged <mongedmostafa19@gmail.com>
2024-04-20 01:21:03 +0200

    using spy rather than mock
    when mocking a function in the same class use spy not mock

--- src/test/java/Project/MongedInputTest.java
+++ src/test/java/Project/MongedInputTest.java
@@ -5,92 +5,91 @@ import org.junit.jupiter.api.Test;
 import org.project.InputValidator;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 public class MongedInputTest {
     /*                              isFinalExamMarkValid Method Testing                         */
     @Test
     void isFinalExamMarkValid_validMark_inRange(){
-        InputValidator mockValidateRange = mock(InputValidator.class);
-        InputValidator in_validator =new InputValidator();
+        InputValidator in_validator = new InputValidator();
+        InputValidator spyValidator = spy(in_validator);
         int min =0;
         int max =60;
         int input = 10;
-        when(mockValidateRange.validate_range(min,max,input)).thenReturn(true );
-        boolean actual = in_validator.isFinalExamMarkValid(String.valueOf(input));
+        when(spyValidator.validate_range(min,max,input)).thenReturn(true );
+        boolean actual = spyValidator.isFinalExamMarkValid(String.valueOf(input));
         assertTrue(actual);
     }
 
     @Test
     void isFinalExamMarkValid_validMark_maxValue(){
-        InputValidator mockValidateRange = mock(InputValidator.class);
-        InputValidator in_validator =new InputValidator();
+        InputValidator in_validator = new InputValidator();
+        InputValidator spyValidator = spy(in_validator);
         int min =0;
         int max =60;
         int input = 60;
-        when(mockValidateRange.validate_range(min,max,input)).thenReturn(true );
-        boolean actual = in_validator.isFinalExamMarkValid(String.valueOf(input));
+        when(spyValidator.validate_range(min,max,input)).thenReturn(true );
+        boolean actual = spyValidator.isFinalExamMarkValid(String.valueOf(input));
         assertTrue(actual);
     }
 
     @Test
     void isFinalExamMarkValid_validMark_minValue(){
-        InputValidator mockValidateRange = mock(InputValidator.class);
-        InputValidator in_validator =new InputValidator();
+        InputValidator in_validator = new InputValidator();
+        InputValidator spyValidator = spy(in_validator);
         int min =0;
         int max =60;
         int input = 0;
-        when(mockValidateRange.validate_range(min,max,input)).thenReturn(true );
-        boolean actual = in_validator.isFinalExamMarkValid(String.valueOf(input));
+        when(spyValidator.validate_range(min,max,input)).thenReturn(true );
+        boolean actual = spyValidator.isFinalExamMarkValid(String.valueOf(input));
         assertTrue(actual);
     }
 
     @Test
     void isFinalExamMarkValid_invalidMark_smallerValueByOne(){
-        InputValidator mockValidateRange = mock(InputValidator.class);
-        InputValidator in_validator =new InputValidator();
+        InputValidator in_validator = new InputValidator();
+        InputValidator spyValidator = spy(in_validator);
         int min =0;
         int max =60;
         int input = -1;
-        when(mockValidateRange.validate_range(min,max,input)).thenReturn(false );
-        boolean actual = in_validator.isFinalExamMarkValid(String.valueOf(input));
+        when(spyValidator.validate_range(min,max,input)).thenReturn(false );
+        boolean actual = spyValidator.isFinalExamMarkValid(String.valueOf(input));
         assertFalse(actual);
     }
 
     @Test
     void isFinalExamMarkValid_invalidMark_greaterValueByOne(){
-        InputValidator mockValidateRange = mock(InputValidator.class);
-        InputValidator in_validator =new InputValidator();
+        InputValidator in_validator = new InputValidator();
+        InputValidator spyValidator = spy(in_validator);
         int min =0;
         int max =60;
         int input = 61;
-        when(mockValidateRange.validate_range(min,max,input)).thenReturn(false );
-        boolean actual = in_validator.isFinalExamMarkValid(String.valueOf(input));
+        when(spyValidator.validate_range(min,max,input)).thenReturn(false );
+        boolean actual = spyValidator.isFinalExamMarkValid(String.valueOf(input));
         assertFalse(actual);
     }
 
     @Test
     void isFinalExamMarkValid_invalidMark_biggerValueThanMax(){
-        InputValidator mockValidateRange = mock(InputValidator.class);
-        InputValidator in_validator =new InputValidator();
+        InputValidator in_validator = new InputValidator();
+        InputValidator spyValidator = spy(in_validator);
         int min =0;
         int max =60;
         int input = 100;
-        when(mockValidateRange.validate_range(min,max,input)).thenReturn(true );
-        boolean actual = in_validator.isFinalExamMarkValid(String.valueOf(input));
+        when(spyValidator.validate_range(min,max,input)).thenReturn(false );
+        boolean actual = spyValidator.isFinalExamMarkValid(String.valueOf(input));
         assertFalse(actual);
     }
 
     @Test
     void isFinalExamMarkValid_invalidMark_smallerValue(){
-        InputValidator mockValidateRange = mock(InputValidator.class);
-        InputValidator in_validator =new InputValidator();
+        InputValidator in_validator = new InputValidator();
+        InputValidator spyValidator = spy(in_validator);
         int min =0;
         int max =60;
         int input = -100;
-        when(mockValidateRange.validate_range(min,max,input)).thenReturn(false );
-        boolean actual = in_validator.isFinalExamMarkValid(String.valueOf(input));
+        when(spyValidator.validate_range(min,max,input)).thenReturn(false );
+        boolean actual = spyValidator.isFinalExamMarkValid(String.valueOf(input));
         assertFalse(actual);
     }
 
@@ -106,85 +105,85 @@ public class MongedInputTest {
     /*                              isMidtermExamMarkValid Method Testing                         */
     @Test
     void isMidtermExamMarkValid_validMark_inRange(){
-        InputValidator mockValidateRange = mock(InputValidator.class);
-        InputValidator in_validator =new InputValidator();
+        InputValidator in_validator = new InputValidator();
+        InputValidator spyValidator = spy(in_validator);
         int min =0;
         int max =20;
         int input = 10;
-        when(mockValidateRange.validate_range(min,max,input)).thenReturn(true );
-        boolean actual = in_validator.isMidtermExamMarkValid(String.valueOf(input));
+        when(spyValidator.validate_range(min,max,input)).thenReturn(true );
+        boolean actual = spyValidator.isMidtermExamMarkValid(String.valueOf(input));
         assertTrue(actual);
     }
 
     @Test
     void isMidtermExamMarkValid_validMark_maxValue(){
-        InputValidator mockValidateRange = mock(InputValidator.class);
-        InputValidator in_validator =new InputValidator();
+        InputValidator in_validator = new InputValidator();
+        InputValidator spyValidator = spy(in_validator);
         int min =0;
         int max =20;
         int input = 20;
-        when(mockValidateRange.validate_range(min,max,input)).thenReturn(true );
-        boolean actual = in_validator.isMidtermExamMarkValid(String.valueOf(input));
+        when(spyValidator.validate_range(min,max,input)).thenReturn(true );
+        boolean actual = spyValidator.isMidtermExamMarkValid(String.valueOf(input));
         assertTrue(actual);
     }
 
     @Test
     void isMidtermExamMarkValid_validMark_minValue(){
-        InputValidator mockValidateRange = mock(InputValidator.class);
-        InputValidator in_validator =new InputValidator();
+        InputValidator in_validator = new InputValidator();
+        InputValidator spyValidator = spy(in_validator);
         int min =0;
         int max =20;
         int input = 0;
-        when(mockValidateRange.validate_range(min,max,input)).thenReturn(true );
-        boolean actual = in_validator.isMidtermExamMarkValid(String.valueOf(input));
+        when(spyValidator.validate_range(min,max,input)).thenReturn(true );
+        boolean actual = spyValidator.isMidtermExamMarkValid(String.valueOf(input));
         assertTrue(actual);
     }
 
     @Test
     void isMidtermExamMarkValid_invalidMark_smallerValueByOne(){
-        InputValidator mockValidateRange = mock(InputValidator.class);
-        InputValidator in_validator =new InputValidator();
+        InputValidator in_validator = new InputValidator();
+        InputValidator spyValidator = spy(in_validator);
         int min =0;
         int max =20;
         int input = -1;
-        when(mockValidateRange.validate_range(min,max,input)).thenReturn(false );
-        boolean actual = in_validator.isMidtermExamMarkValid(String.valueOf(input));
+        when(spyValidator.validate_range(min,max,input)).thenReturn(false );
+        boolean actual = spyValidator.isMidtermExamMarkValid(String.valueOf(input));
         assertFalse(actual);
     }
 
     @Test
     void isMidtermExamMarkValid_invalidMark_greaterValueByOne(){
-        InputValidator mockValidateRange = mock(InputValidator.class);
-        InputValidator in_validator =new InputValidator();
+        InputValidator in_validator = new InputValidator();
+        InputValidator spyValidator = spy(in_validator);
         int min =0;
         int max =20;
         int input = 21;
-        when(mockValidateRange.validate_range(min,max,input)).thenReturn(false );
-        boolean actual = in_validator.isMidtermExamMarkValid(String.valueOf(input));
+        when(spyValidator.validate_range(min,max,input)).thenReturn(false );
+        boolean actual = spyValidator.isMidtermExamMarkValid(String.valueOf(input));
         assertFalse(actual);
     }
 
     @Test
     void isMidtermExamMarkValid_invalidMark_biggerValue(){
-        InputValidator mockValidateRange = mock(InputValidator.class);
-        InputValidator in_validator =new InputValidator();
+        InputValidator in_validator = new InputValidator();
+        InputValidator spyValidator = spy(in_validator);
         int min =0;
         int max =20;
         int input = 100;
-        when(mockValidateRange.validate_range(min,max,input)).thenReturn(false );
-        boolean actual = in_validator.isMidtermExamMarkValid(String.valueOf(input));
+        when(spyValidator.validate_range(min,max,input)).thenReturn(false );
+        boolean actual = spyValidator.isMidtermExamMarkValid(String.valueOf(input));
         assertFalse(actual);
     }
 
     @Test
     void isMidtermExamMarkValid_invalidMark_smallerValue(){
-        InputValidator mockValidateRange = mock(InputValidator.class);
-        InputValidator in_validator =new InputValidator();
+        InputValidator in_validator = new InputValidator();
+        InputValidator spyValidator = spy(in_validator);
         int min =0;
         int max =20;
         int input = -100;
-        when(mockValidateRange.validate_range(min,max,input)).thenReturn(false );
-        boolean actual = in_validator.isMidtermExamMarkValid(String.valueOf(input));
+        when(spyValidator.validate_range(min,max,input)).thenReturn(false );
+        boolean actual = spyValidator.isMidtermExamMarkValid(String.valueOf(input));
         assertFalse(actual);
     }
 
