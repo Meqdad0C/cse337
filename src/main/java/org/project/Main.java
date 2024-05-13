f0d87662d264f61db78057ccb44ac2c05bf7b9aa
Meqdad Amr <Meqdad_@outlook.com>
2024-05-13 19:55:15 +0300

    refactor main driver program

--- src/main/java/org/project/Main.java
+++ src/main/java/org/project/Main.java
@@ -6,16 +6,6 @@ import java.nio.file.Path;
 import java.util.List;
 
 public class Main {
-    static StringBuilder parse(List<String> lines) {
-        StringBuilder output_string = new StringBuilder();
-        Subject subject = InputParser.parseSubject(lines.removeFirst());
-        output_string.append(OutputWriter.writeSubject(subject));
-        output_string.append(OutputWriter.writeStudentHeader());
-        List<Student> students = lines.stream().map(InputParser::parseStudent).toList();
-        students.forEach(student -> output_string.append(OutputWriter.writeStudent(student)));
-        return output_string;
-    }
-
     public static void main(String[] args) throws IOException {
         int argc = args.length;
         if (argc != 2) {
@@ -28,10 +18,20 @@ public class Main {
         FileReader fileReader = new FileReader(input_file);
         lines = fileReader.read_file_lines();
 
-        // Processing the content of the file
-        StringBuilder output_string = parse(lines);
+        StringBuilder output_string = new StringBuilder();
+        try {
+            // Processing the content of the file
+            Subject subject = InputParser.parseSubject(lines.removeFirst());
+            output_string.append(OutputWriter.writeSubject(subject));
+            output_string.append(OutputWriter.writeStudentHeader());
+            List<Student> students = lines.stream().map(InputParser::parseStudent).toList();
+            students.forEach(student -> output_string.append(OutputWriter.writeStudent(student)));
 
-        // Writing new content to an output file
-        Files.writeString(Path.of(args[1]), output_string);
+            // Writing new content to an output file
+            Files.writeString(Path.of(args[1]), output_string);
+        } catch (IllegalArgumentException e) {
+            System.err.println(e.getMessage());
+            throw e;
+        }
     }
 }
